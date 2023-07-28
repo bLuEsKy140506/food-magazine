be5eb59c7c921f03a09032bb125eca16addf2469
bLuEsKy140506 <115715458+bLuEsKy140506@users.noreply.github.com>
2023-07-28 15:57:22 +0800

    feature-recipe-details

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,15 @@ import {
 
 import RouteLayout from "./layouts/RouteLayout";
 import RecipeList from "./pages/RecipeList";
+import RecipeDetails from "./pages/RecipeDetails";
+
 import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RouteLayout />}>
       <Route index element={<RecipeList />} />
+      <Route path="recipes/sample" element={<RecipeDetails />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
